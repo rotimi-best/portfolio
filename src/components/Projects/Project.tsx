6b75dc40ade3b84733f6bbb1b565df9157b32a62
Best <irb.ossystem@gmail.com>
2019-11-29 12:50:09 +0200

    feat: style work boxes and added images

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -4,14 +4,21 @@ import { FaLongArrowAltRight } from 'react-icons/fa'
 type Props = {
   name: string,
   techStack: string,
+  imageSrc: string,
 }
 
 export default function Project(props: Props) {
   return (
     <div className="Project">
-      <h4 className="ProjectName">{props.name}</h4>
-      <p className="ProjectTechStack">{props.techStack}</p>
-      <FaLongArrowAltRight className="ArrowRight"/>
+      <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank" rel="noopener noreferrer">
+        <div className="ProjectOverlay"></div>
+        <img className="ProjectImage" alt="ProjectImage" src={props.imageSrc} />
+        <div className="ProjectDetails FadeInBottom">
+          <h4 className="ProjectName">{props.name}</h4>
+          <p className="ProjectTechStack">{props.techStack}</p>
+          <FaLongArrowAltRight className="ArrowRight"/>
+        </div>
+      </a>
     </div>
   )
 }
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import Project from './Project'
 
 import './Projects.css'
@@ -8,51 +7,55 @@ type Project = {
   key: number,
   name: string,
   techStack: string,
+  imageSrc: string,
 }
 
 const projects: Project[] = [
   {
     key: 1,
     name: 'Linkedin Automation Tool',
-    techStack: 'Node.js, MongoDB'
+    techStack: 'Node.js, MongoDB',
+    imageSrc: 'https://os-system.com/vendors/images/main-portfolio/enjoy/Enjoy_2.jpg',
   },
   {
     key: 2,
     name: 'Linkedin Automation Tool',
-    techStack: 'Node.js, MongoDB'
+    techStack: 'Node.js, MongoDB',
+    imageSrc: 'https://os-system.com/vendors/images/main-portfolio/illich-realty/phone-section.png',
   },
   {
     key: 3,
     name: 'Linkedin Automation Tool',
-    techStack: 'Node.js, MongoDB'
+    techStack: 'Node.js, MongoDB',
+    imageSrc: 'https://os-system.com/vendors/images/main-portfolio/illich-realty/solution-picture.png',
   },
   {
     key: 4,
     name: 'Linkedin Automation Tool',
-    techStack: 'Node.js, MongoDB'
+    techStack: 'Node.js, MongoDB',
+    imageSrc: 'https://os-system.com/vendors/images/main-portfolio/gud-job/section_3_1.png',
   },
   {
     key: 5,
     name: 'Linkedin Automation Tool',
-    techStack: 'Node.js, MongoDB'
+    techStack: 'Node.js, MongoDB',
+    imageSrc: 'https://os-system.com/vendors/images/main-portfolio/wikigrads/picture2.png?v2',
   },
 ]
 
 export default class Projects extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
 
   render() {
     return (
       <div className="ProjectsRoot">
         <h1 className="ProjectsTitle">LATEST WORKS</h1>
         <div className="Projects">
-          {projects.map(({key, name, techStack}) => (
+          {projects.map(({key, name, techStack, imageSrc}) => (
             <Project
               key={key}
               name={name}
               techStack={techStack}
+              imageSrc={imageSrc}
             />
           ))}
         </div>
