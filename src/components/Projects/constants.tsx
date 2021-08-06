b20261980187864ec9c4c1271a34463752e36a78
rotimi-best <rotimiibitoyeemma@gmail.com>
2021-08-07 02:26:46 +0300

    Add one more project

--- src/components/Projects/constants.tsx
+++ src/components/Projects/constants.tsx
@@ -1,32 +1,47 @@
-import ProjectType from "./Types";
+import ProjectType from './Types';
 
 export const projects: ProjectType[] = [
   {
     key: 1,
-    name: "Trumpetr",
-    techStacks: ["Node.js", "MongoDB", "Telegram BOT"],
-    image:
-      "https://i.ibb.co/rfGzzXZ/trumpetr.png",
-    src: "https://trumpetr.vercel.app",
-    description: 'Trumpetrs goal is to allow people share what they learnt from the bible with their community. This is to encourage accountability and also to help you learn from other people',
+    name: 'PrayerKeep',
+    techStacks: ['React.js', 'MongoDB Atlas', 'Node.js', 'Heroku'],
+    image: 'https://i.ibb.co/NFkDkdh/prayer-keep.png',
+    src: 'https://coda.io/d/PrayerKeep-Better-way-of-Sharing-Prayer-Requests_dEA67Yo8TwX/PrayerKeep-A-Better-way-of-Sharing-Prayer-Requests_susy7#_luWvs',
+    description:
+      'Prayerkeep is a collaborative PWA, that helps people share prayer requests with others. You can also create a collection of prayer requests you and your friends can pray together about',
   },
   {
     key: 2,
-    name: "PrayerKeep",
-    techStacks: ["Node.js", "MongoDB", "Telegram BOT"],
-    image:
-      "https://i.ibb.co/NFkDkdh/prayer-keep.png",
-    src: "https://coda.io/d/PrayerKeep-Better-way-of-Sharing-Prayer-Requests_dEA67Yo8TwX/PrayerKeep-A-Better-way-of-Sharing-Prayer-Requests_susy7#_luWvs",
-    description: 'Prayerkeep is a collaborative PWA, that helps people share prayer requests with others. You can also create a collection of prayer requests you and your friends can pray together about',
+    name: 'ClassroomIO',
+    techStacks: [
+      'Svelte.js',
+      'Supabase',
+      'Telegram BOT',
+      'Golang',
+      'TailwindCSS',
+    ],
+    image: 'https://i.ibb.co/q5mM4Sr/classroomio-new.png',
+    src: 'https://www.classroomio.com',
+    description:
+      'A web application that empowers classroom to be effective both online and in a classroom. The main goal of this project is to give students access to all they need in the course of studying.',
   },
   {
     key: 3,
-    name: "ClassroomIO",
-    techStacks: ["Node.js", "MongoDB", "Telegram BOT"],
-    image:
-      "https://i.ibb.co/dJwqFXn/classroomio.png",
-    src: "https://www.classroomio.com",
-    description: 'A web application that empowers classroom to be effective both online and in a classroom. The main goal of this project is to give students access to all they need in the course of studying.',
+    name: 'SvelteDoc',
+    techStacks: ['Svelte', 'Supabase', 'Netlify', 'TailwindCSS'],
+    image: 'https://i.ibb.co/JqPFLhT/Svelte-Doc.png',
+    src: 'https://sveltedoc.xyz',
+    description:
+      'Think of Google doc (without the whole collaboration complexity) with a twist that all the documents you create are open to everyone to find and leave comments. Anyone should be able to search and find it online.',
+  },
+  {
+    key: 4,
+    name: 'Trumpetr',
+    techStacks: ['Node.js', 'MongoDB', 'Telegram BOT'],
+    image: 'https://i.ibb.co/rfGzzXZ/trumpetr.png',
+    src: 'https://trumpetr.vercel.app',
+    description:
+      'Trumpetrs goal is to allow people share what they learnt from the bible with their community. This is to encourage accountability and also to help you learn from other people',
   },
   // {
   //   key: 5,
